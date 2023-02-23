1334d6b06c02414a826403a69e6ea6e6ade40624
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-02-23 15:25:28 -0600

    fixes

--- Projects/html-css-js/calculator/script.js
+++ Projects/html-css-js/calculator/script.js
@@ -74,22 +74,70 @@ function calculate() {
     var resString = Array.from(String(res));
     var numLeftDecimal = Math.trunc(res);
     var spaceLeft = Array.from(String(numLeftDecimal)).length;
-    if (resString.includes(".")==false) {
-        if (spaceLeft > 9)  {
+    if (resString.includes(".") == false) {
+        if (spaceLeft > 9) {
             display.innerHTML = "Too long!"
         }
         else display.innerHTML = res + ".";
     }
-    var truncNum = Number(res.toFixed((9-spaceLeft)));
+    var truncNum = Number(res.toFixed((9 - spaceLeft)));
     console.log(resString);
     console.log(spaceLeft);
     if (resString.includes(".")) {
         if (resString.length > 9) {
             display.innerHTML = truncNum;
         }
-        else if(resString.length > 9 && spaceLeft > 9)  {
+        else if (resString.length > 9 && spaceLeft > 9) {
             display.innerHTML = "Too long!"
         }
         else display.innerHTML = res;
+    }
+}
+
+
+
+
+
+//The coding below provided by: https://www.w3schools.com/howto/howto_js_draggable.asp
+dragElement(document.getElementById("myCalculator"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "main")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "main").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
     }
 }
