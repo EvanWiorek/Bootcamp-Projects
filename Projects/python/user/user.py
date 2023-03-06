65e32540d6a7c350be5859caa1a185634a0be634
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-03-06 09:38:57 -0600

    update

--- Projects/python/user/user.py
+++ Projects/python/user/user.py
@@ -10,6 +10,7 @@ class User:
     def display_info(self):
         for attribute, value in self.__dict__.items():
             print(str(attribute)+ ": " + str(value))
+        return self
 
     def enroll(self):
         if self.is_rewards_member == True:
@@ -18,6 +19,7 @@ class User:
         else: 
             self.is_rewards_member = True
             self.gold_card_points = 200
+        return self
     
     def spend_points(self, amount):
         if amount > self.gold_card_points:
@@ -25,14 +27,15 @@ class User:
         else:
             self.gold_card_points -= amount
             print(f"Thank you for your purchase! You have {self.gold_card_points} Gold Card Points remaining.")
-
+        return self
 
 user_1 = User("Evan", "Wiorek", "wiorek.evan@gmail.com", 26)
-user_1.enroll()
-user_1.spend_points(300)
-user_1.spend_points(150)
-user_1.spend_points(300)
-user_1.display_info()
-user_1.enroll()
+# user_1.enroll()
+# user_1.spend_points(300)
+# user_1.spend_points(150)
+# user_1.spend_points(300)
+# user_1.display_info()
+# user_1.enroll()
+user_1.enroll().spend_points(300).spend_points(150).spend_points(300).display_info().enroll()
 
 user_2 = User("John", "Jones", "realemail@realemail.com", 297)
