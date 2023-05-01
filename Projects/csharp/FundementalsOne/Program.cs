518590986b1f8c7c4bd7647e78e5182f8d968397
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-05-01 12:59:04 -0500

    FundamentalsOne

---
+++ Projects/csharp/FundementalsOne/Program.cs
@@ -0,0 +1,56 @@
+﻿//1
+for(int i = 1; i<256; i++)
+{
+  // Console.WriteLine(i);
+}
+
+//2
+Random rand = new Random();
+for(int i = 1; i<6; i++)
+{
+  // Console.WriteLine(rand.Next(10,21));
+}
+
+//3
+int sum = 0;
+for(int i = 1; i<256; i++)
+{
+  sum += i;
+}
+// Console.WriteLine(sum);
+
+//4
+for(int i = 1; i<101; i++)
+{
+  if(i % 3 == 0 || i % 5 == 0)
+  {
+    // Console.WriteLine(i);
+  }
+}
+
+//5
+for(int i = 1; i<101; i++)
+{
+  if(i % 3 == 0)
+  {
+    // Console.WriteLine($"Fizz {i}");
+  }
+  else if( i % 5 == 0)
+  {
+    // Console.WriteLine($"Buzz {i}");
+  }
+}
+
+//6
+for(int i = 1; i<101; i++)
+{
+  if(i % 3 == 0 && i % 5 == 0)
+  {
+    Console.WriteLine($"FizzBuzz {i}");
+  }
+  else if(i % 3 == 0) {
+    Console.WriteLine($"Fizz {i}");
+  }
+  else if(i % 5 == 0)
+  {Console.WriteLine($"Buzz {i}");}
+}
